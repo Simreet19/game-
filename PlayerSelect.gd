b6c4cacc31a5acc19f7f2760c3b5cc1380742f0e
Simreet <simreetgoraya19@gmail.com>
2024-10-20 01:03:02 +0530

    Add files via upload

--- PlayerSelect.gd
+++ PlayerSelect.gd
@@ -3,11 +3,13 @@ extends Control
 
 # Preload your background textures
 var backgrounds = [
-	preload("res://Assets/characters/download.jpeg"),
-	preload("res://Assets/bg/WhatsApp Image 2024-10-19 at 4.51.51 PM.jpeg"),
-	preload("res://Assets/characters/p1.jpg")
+	preload("res://Assets/bg11.jpg"),
+	#preload("res://Assets/bg/WhatsApp Image 2024-10-19 at 4.51.51 PM.jpeg"),
+	#preload("res://Assets/characters/p1.jpg")
 ]
-
+@onready var animated_sprite_2d = $Panel/AnimatedSprite2D
+@onready var animated_sprite_2d2 = $Panel/AnimatedSprite2D2
+@onready var animated_sprite_2d3 = $Panel/AnimatedSprite2D3
 @onready var player_1 = $Player1
 @onready var player_2 = $Player2
 @onready var player_3 = $Player3
@@ -21,17 +23,29 @@ func _ready():
 	player_1.pressed.connect(self._on_Button_pressed_1)
 	player_2.pressed.connect(self._on_Button_pressed_2)
 	player_3.pressed.connect(self._on_Button_pressed_3)
-
+	animated_sprite_2d.hide()
+	animated_sprite_2d2.hide()
+	animated_sprite_2d3.hide()
+	
 
 
 func _on_Button_pressed_1():
 	# Change to the first background
-	texture_rect.texture = backgrounds[0]
+	animated_sprite_2d.show();
+	animated_sprite_2d.play()
+	animated_sprite_2d2.hide()
+	animated_sprite_2d3.hide()
 
 func _on_Button_pressed_2():
 	# Change to the second background
-	texture_rect.texture = backgrounds[1]
+	animated_sprite_2d3.show();
+	animated_sprite_2d3.play()
+	animated_sprite_2d.hide()
+	animated_sprite_2d2.hide()
 
 func _on_Button_pressed_3():
 	# Change to the third background
-	texture_rect.texture = backgrounds[2]
+	animated_sprite_2d2.show();
+	animated_sprite_2d2.play()
+	animated_sprite_2d.hide()
+	animated_sprite_2d3.hide()
